8a37abbc7fc62cdbd75d44810c3119479e233eb5
Judy McGoldrick <jjeanius@verizon.net>
2018-05-27 14:40:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,6 @@
 class Application
-
+  
+  @@cart = Array.new
   @@items = ["Apples","Carrots","Pears"]
 
   def call(env)
@@ -9,22 +10,43 @@ class Application
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
-      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      resp.write dispay_cart
+    elsif req.path.match(/add/)
+      item_to_add = req.param["item"]
+      resp.write add_item(item_to_add)
     else
       resp.write "Path Not Found"
     end
-
-    resp.finish
+      return "Couldn't find #{search_term}"
+      end
+    end
   end
 
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
     else
-      return "Couldn't find #{search_term}"
+      puts "Couldn't find #{search_term}"
     end
   end
-end
+
+  def dispay_cart
+    if @@cart.empty?
+      return "cart is empty"
+    else
+      return "Your cart contains #{@@cart.join("\n")}"
+    end
+  end
+
+  def add_item(item_to_add)
+    if @@cart.include?(item_to_add)
+      @@cart << item_to_add
+      return "#{item_to_add} has been added to the cart"
+    else
+      return "We don't have that item"
+    end
+  end
