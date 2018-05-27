755fd6d1202d51bf66e9492a20aba5975916c04f
Judy McGoldrick <jjeanius@verizon.net>
2018-05-27 15:20:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,8 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
+      @@items.each do |items|
+        resp.write "#{items}\n"
       end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
